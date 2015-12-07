ba7670ce5543eb2163df3ea8da6f75661f690f70
Your Name <you@example.com>
2015-12-07 11:15:35 +0000

    feature: It can now return a Dynarex document of the given weekly-planner.txt file.

--- lib/weekly_planner.rb
+++ lib/weekly_planner.rb
@@ -14,19 +14,23 @@ class WeeklyPlanner
     
     @filename, @path = filename, path
     
-    if File.exists? File.join(path, filename) then
-      @to_s = File.read File.join(path, filename)
-    else
-      @to_s = create()
-    end
+    fpath = File.join(path, filename)
+    @s = File.exists?(fpath) ? File.read(fpath) :  create()    
+    @dx = create_dx(@s)
+    
   end
 
   def dx()
+    @dx
   end
   
   def save(filename=@filename)
-    s = File.basename(filename) + "\n" + @to_s.lines[1..-1].join
+    s = File.basename(filename) + "\n" + @s.lines[1..-1].join
     File.write File.join(@path, filename), s
+  end
+  
+  def to_s()
+    @s
   end
 
   private
@@ -46,8 +50,34 @@ class WeeklyPlanner
     a += (d+1).upto(d+6).map {|date|  Date::ABBR_DAYNAMES[date.wday] }
 
     a2 = a.map {|x| "%s\n%s" % [x, '-' * x.length]}
-    s = File.basename(@filename) + "\n==================\n\n%s\n\n" % 
-                                                              [a2.join("\n\n")]
+    title = File.basename(@filename)
+    s = title + "\n" + "=" * title.length + "\n\n%s\n\n" % [a2.join("\n\n")]
+  end
+  
+  def create_dx(s)
+    
+    rows = s.split(/.*(?=^[\w, ]+\n\-+)/)
+
+    dx = Dynarex.new 'sections[title]/section(x)'
+
+    rows.shift
+    d = Date.parse(rows.first.lines.first.chomp)
+    dx.title = "Weekly Planner (%s)" % (d).strftime("%d-%b-%Y")
+
+    rows.each.with_index do |raw_x, i|
+      
+      a = raw_x.lines
+      a.shift
+      heading = "# %s\n" % (d + i).strftime("%d-%b-%Y")
+
+      a.shift # removes the dashed line
+      content = a.join
+      dx.create({x: heading + content}, id: (d + i).strftime("%Y%m%d"))
+
+    end
+    
+    return dx
+    
   end
   
   def parse()
